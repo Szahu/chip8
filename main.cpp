528a84e6789094cf4ca0107017bd0a656c8c1774
stanislaw <szahuyt@gmail.com>
2023-09-23 00:57:31 +0200

    add keypad support

--- main.cpp
+++ main.cpp
@@ -1,66 +1,62 @@
-#include <iostream>
-#include <stack>
-#include <thread>
 #include <chrono>
 #include <cstdlib>
-#include <string>
 #include <cstring>
 #include <fstream>
+#include <iostream>
+#include <mutex>
+#include <stack>
+#include <string>
+#include <thread>
+#include <unordered_map>
 
 #include "SDL3/SDL.h"
 
-void print_hex(const uint16_t& val) {
-    printf("%4X\n", val);
-}
+void print_hex(const uint16_t &val) { printf("%4X\n", val); }
 
 // only works on WSL
-void play_beep(const unsigned int& ms) {
-    const std::string command = "powershell.exe '[console]::beep(261.6," + std::to_string(ms) +")'";
+void play_beep(const unsigned int &ms) {
+    const std::string command =
+        "powershell.exe '[console]::beep(261.6," + std::to_string(ms) + ")'";
     std::system(command.c_str());
 }
 
 #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)   \
-  ((byte) & 0x80 ? '1' : '0'), \
-  ((byte) & 0x40 ? '1' : '0'), \
-  ((byte) & 0x20 ? '1' : '0'), \
-  ((byte) & 0x10 ? '1' : '0'), \
-  ((byte) & 0x08 ? '1' : '0'), \
-  ((byte) & 0x04 ? '1' : '0'), \
-  ((byte) & 0x02 ? '1' : '0'), \
-  ((byte) & 0x01 ? '1' : '0') 
+#define BYTE_TO_BINARY(byte)                                                                       \
+    ((byte)&0x80 ? '1' : '0'), ((byte)&0x40 ? '1' : '0'), ((byte)&0x20 ? '1' : '0'),               \
+        ((byte)&0x10 ? '1' : '0'), ((byte)&0x08 ? '1' : '0'), ((byte)&0x04 ? '1' : '0'),           \
+        ((byte)&0x02 ? '1' : '0'), ((byte)&0x01 ? '1' : '0')
 
-void print_byte(const char byte) {
-    printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(byte));
-}
+void print_byte(const char byte) { printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(byte)); }
 
-void print_display(const unsigned char* display, const unsigned int display_width, const unsigned int display_height) {
-    for (unsigned int i = 0;i < display_height; ++i) {
-        for (unsigned int j = 0;j < display_width / 8; ++j) {
+void print_display(const unsigned char *display, const unsigned int display_width,
+                   const unsigned int display_height) {
+    for (unsigned int i = 0; i < display_height; ++i) {
+        for (unsigned int j = 0; j < display_width / 8; ++j) {
             printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(display[i * display_width / 8 + j]));
         }
         printf("\n");
     }
 }
 
-void print_registers(const uint8_t* regs) {
-    for (int i = 0;i < 16; ++i) {
+void print_registers(const uint8_t *regs) {
+    for (int i = 0; i < 16; ++i) {
         printf("register %X: %X\n", i, regs[i]);
     }
 }
 
-bool load_program_from_file(const std::string& filename, char* mem, const unsigned int& pc) {
+bool load_program_from_file(const std::string &filename, char *mem, const unsigned int &pc) {
     std::ifstream file(filename, std::ios::binary | std::ios::ate);
     std::streamsize size = file.tellg();
     file.seekg(0, std::ios::beg);
-    return (bool)file.read(reinterpret_cast<char*>(mem + pc), size);
+    return (bool)file.read(reinterpret_cast<char *>(mem + pc), size);
 }
 
-void chip_display_to_pixel_array(const unsigned char* chip_display, unsigned char* pixel_array,
-                                 const unsigned int display_width, const unsigned int display_height) {
+void chip_display_to_pixel_array(const unsigned char *chip_display, unsigned char *pixel_array,
+                                 const unsigned int display_width,
+                                 const unsigned int display_height) {
 
-    for (unsigned int i = 0;i < display_width * display_height / 8; ++i) {
-        for (unsigned int j = 0 ;j < 8; ++j) {
+    for (unsigned int i = 0; i < display_width * display_height / 8; ++i) {
+        for (unsigned int j = 0; j < 8; ++j) {
             char mask = 0b10000000 >> j;
             unsigned char pixel = 0x00;
 
@@ -73,10 +69,10 @@ void chip_display_to_pixel_array(const unsigned char* chip_display, unsigned cha
             pixel_array[3 * (i * 8 + j) + 2] = pixel;
         }
     }
-
 }
 
-void sdl_thread_fun(unsigned char* chip_display, bool* terminate) {
+void sdl_thread_fun(unsigned char *chip_display, bool *terminate, uint8_t *keys_pressed,
+                    bool *waiting_for_key, uint8_t *last_key_pressed, std::mutex *key_mutex) {
 
     const unsigned int SDL_FPS = 60;
 
@@ -85,18 +81,48 @@ void sdl_thread_fun(unsigned char* chip_display, bool* terminate) {
         return;
     }
 
-    SDL_Window* window = SDL_CreateWindow("CHIP8 emulator", 640, 320, 0);
+    SDL_Window *window = SDL_CreateWindow("CHIP8 emulator", 640, 320, 0);
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL, 0);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL, 0);
 
     SDL_Event event;
 
     unsigned char pixel_array[64 * 32 * 3];
 
+    std::unordered_map<int32_t, uint8_t> key_to_hex = {
+        {SDLK_x, 0x0}, {SDLK_1, 0x1}, {SDLK_2, 0x2}, {SDLK_3, 0x3}, {SDLK_q, 0x4}, {SDLK_w, 0x5},
+        {SDLK_e, 0x6}, {SDLK_a, 0x7}, {SDLK_s, 0x8}, {SDLK_d, 0x9}, {SDLK_z, 0xA}, {SDLK_c, 0xB},
+        {SDLK_4, 0xC}, {SDLK_r, 0xD}, {SDLK_f, 0xE}, {SDLK_v, 0xF},
+    };
+
     while (!(*terminate)) {
 
         while (SDL_PollEvent(&event)) {
+
             *terminate = event.type == SDL_EVENT_QUIT;
+
+            if (event.type == SDL_EVENT_KEY_DOWN) {
+
+                std::lock_guard<std::mutex> lock(*key_mutex);
+                auto itr = key_to_hex.find(event.key.keysym.sym);
+
+                if (itr != key_to_hex.end()) {
+                    keys_pressed[itr->second] = 1;
+                }
+
+            } else if (event.type == SDL_EVENT_KEY_UP) {
+
+                std::lock_guard<std::mutex> lock(*key_mutex);
+                auto itr = key_to_hex.find(event.key.keysym.sym);
+
+                if (itr != key_to_hex.end()) {
+                    keys_pressed[itr->second] = 0;
+                    if (*waiting_for_key && *last_key_pressed == 0xFF) {
+                        *waiting_for_key = false;
+                        *last_key_pressed = itr->second;
+                    }
+                }
+            }
         }
 
         // Clear the renderer
@@ -104,15 +130,18 @@ void sdl_thread_fun(unsigned char* chip_display, bool* terminate) {
         SDL_RenderClear(renderer);
 
         chip_display_to_pixel_array(chip_display, pixel_array, 64, 32);
-        SDL_Surface* surface = SDL_CreateSurfaceFrom(reinterpret_cast<void*>(pixel_array), 64, 32, 3 * 64, SDL_PixelFormatEnum::SDL_PIXELFORMAT_RGB24);
-        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_Surface *surface =
+            SDL_CreateSurfaceFrom(reinterpret_cast<void *>(pixel_array), 64, 32, 3 * 64,
+                                  SDL_PixelFormatEnum::SDL_PIXELFORMAT_RGB24);
+        SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-        SDL_RenderTexture(renderer, texture, NULL, NULL); 
+        SDL_RenderTexture(renderer, texture, NULL, NULL);
 
         // Update the screen
         SDL_RenderPresent(renderer);
 
-        std::this_thread::sleep_for(std::chrono::milliseconds((unsigned int)((1.0f / SDL_FPS) * 1000.0f)));
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds((unsigned int)((1.0f / SDL_FPS) * 1000.0f)));
     }
 
     SDL_DestroyRenderer(renderer);
@@ -120,17 +149,17 @@ void sdl_thread_fun(unsigned char* chip_display, bool* terminate) {
     SDL_Quit();
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
     std::cout << "Running the emulator!" << std::endl;
 
-    constexpr unsigned int MEM_SIZE = 4096; 
+    constexpr unsigned int MEM_SIZE = 4096;
     constexpr unsigned int DISPLAY_WIDTH = 64;
     constexpr unsigned int DISPLAY_HEIGHT = 32;
     constexpr unsigned int PC_START_POS = 0x200;
     constexpr unsigned int DELEY_TIMER_FPS = 60;
     constexpr unsigned int SOUND_TIMER_FPS = 60;
-    constexpr unsigned int CLOCK_FPS = 10;
+    constexpr unsigned int CLOCK_FPS = 1000000;
     constexpr uint16_t INSTR_TERMIANTE = 0xFFFF;
     constexpr bool USE_OLD_BIT_SHIFT = false;
 
@@ -157,13 +186,17 @@ int main(int argc, char** argv) {
     unsigned char mem[MEM_SIZE];
     unsigned int program_counter = PC_START_POS;
     std::stack<uint16_t> stack;
-    uint8_t delay_timer = 0;
-    uint8_t sound_timer = 0;
+    std::atomic<uint8_t> delay_timer = 0;
+    std::atomic<uint8_t> sound_timer = 0;
 
     uint8_t regs[16];
     std::memset(regs, 0x00, 16);
 
     uint16_t index_register = 0;
+
+    uint8_t keys_pressed[16];
+    uint8_t last_key_pressed = 0xFF;
+    memset(keys_pressed, 0, sizeof(keys_pressed));
 
     unsigned char display[DISPLAY_WIDTH * DISPLAY_HEIGHT / 8];
     std::memset(display, 0x00, DISPLAY_WIDTH * DISPLAY_HEIGHT / 8);
@@ -171,43 +204,47 @@ int main(int argc, char** argv) {
     std::memcpy(mem + FONT_LOC, FONT, sizeof(FONT));
 
     bool terminate = false;
+    bool waiting_for_key = false;
 
-    std::thread deley_timer_thread([&] () {
+    std::thread deley_timer_thread([&]() {
         while (!terminate) {
             if (delay_timer > 0) {
                 delay_timer--;
             }
-            else {
-                delay_timer = 60;
-            }
-            std::this_thread::sleep_for(std::chrono::milliseconds((unsigned int)((1.0f / DELEY_TIMER_FPS) * 1000.0f)));
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds((unsigned int)((1.0f / DELEY_TIMER_FPS) * 1000.0f)));
         }
     });
 
-    std::thread sound_timer_thread([&] () {
+    std::thread sound_timer_thread([&]() {
         while (!terminate) {
             if (sound_timer > 0) {
                 sound_timer--;
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds((unsigned int)((1.0f / SOUND_TIMER_FPS) * 1000.0f)));
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds((unsigned int)((1.0f / SOUND_TIMER_FPS) * 1000.0f)));
         }
     });
 
-    std::thread sound_thread([&] () {
+    std::thread sound_thread([&]() {
         while (!terminate) {
             if (sound_timer > 0) {
                 play_beep((unsigned int)((1.0f / SOUND_TIMER_FPS) * 1000) * sound_timer);
             }
-            std::this_thread::sleep_for(std::chrono::milliseconds((unsigned int)((1.0f / SOUND_TIMER_FPS) * 1000.0f)));
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds((unsigned int)((1.0f / SOUND_TIMER_FPS) * 1000.0f)));
         }
     });
 
-    if (!load_program_from_file(argv[1], reinterpret_cast<char*>(mem), program_counter)) {
+    if (!load_program_from_file(argv[1], reinterpret_cast<char *>(mem), program_counter)) {
         printf("Failed to load the program, from path %s.\n", argv[1]);
         return 1;
     }
 
-    std::thread sdl_thread(sdl_thread_fun, display, &terminate);
+    std::mutex key_mutex;
+
+    std::thread sdl_thread(sdl_thread_fun, display, &terminate, keys_pressed, &waiting_for_key,
+                           &last_key_pressed, &key_mutex);
 
     // main cpu cycle
     while (!terminate) {
@@ -215,7 +252,7 @@ int main(int argc, char** argv) {
         uint16_t instruction = 0;
 
         // fetch
-        instruction = (mem[program_counter] << 8) | mem[program_counter+1];
+        instruction = (mem[program_counter] << 8) | mem[program_counter + 1];
         program_counter += 2;
 
         // decode and execute
@@ -223,11 +260,13 @@ int main(int argc, char** argv) {
         uint8_t Y = (instruction & 0x0F00) >> 8;
         uint8_t Z = (instruction & 0x00F0) >> 4;
         uint8_t W = (instruction & 0x000F);
-        
+
         if (instruction == 0xFFFF) {
             terminate = true;
         } else if (instruction == 0x00E0) {
-            // 00E00 clear screen
+            // 0E00 clear screen
+            memset(display, 0x00, DISPLAY_WIDTH * DISPLAY_HEIGHT / 8);
+
         } else if (X == 0x1) {
 
             // 1NNN -> jump to address NNN
@@ -236,22 +275,26 @@ int main(int argc, char** argv) {
         } else if (X == 0x3) {
 
             // 3XNN -> skip next instruction if regs[X] == NN
-            if (regs[Y] == (instruction & 0x00FF)) program_counter += 2;
+            if (regs[Y] == (instruction & 0x00FF))
+                program_counter += 2;
 
         } else if (X == 0x4) {
 
             // 4XNN -> skip next instruction if regs[X] != NN
-            if (regs[Y] != (instruction & 0x00FF)) program_counter += 2;
+            if (regs[Y] != (instruction & 0x00FF))
+                program_counter += 2;
 
         } else if (X == 0x5 && W == 0x0) {
 
             // 5XY0 -> skip next instruction if regs[X] == regs[Y]
-            if (regs[Y] == regs[Z]) program_counter += 2;
+            if (regs[Y] == regs[Z])
+                program_counter += 2;
 
         } else if (X == 0x9 && W == 0x0) {
 
             // 9XY0 -> skip next instruction if regs[X] != regs[Y]
-            if (regs[Y] != regs[Z]) program_counter += 2;
+            if (regs[Y] != regs[Z])
+                program_counter += 2;
 
         } else if (X == 0x6) {
 
@@ -271,7 +314,7 @@ int main(int argc, char** argv) {
         } else if (X == 0xF && Z == 0x2 && W == 0x9) {
 
             // set index register to font sprite address
-            index_register = FONT_LOC + regs[Y];
+            index_register = FONT_LOC + regs[Y] * 5;
 
         } else if (X == 0x8 && W == 0x0) {
 
@@ -292,6 +335,12 @@ int main(int argc, char** argv) {
 
             // bitwise xor
             regs[Y] = regs[Y] ^ regs[Z];
+
+        } else if (X == 0x8 && W == 0x4) {
+
+            // 8NM4 -> Add regs[N] and regs[M]
+            regs[0xF] = (regs[Y] > 0xFF - regs[Z] || regs[Z] > 0xFF - regs[Y]);
+            regs[Y] = regs[Y] + regs[Z];
 
         } else if (X == 0x8 && W == 0x5) {
 
@@ -329,50 +378,49 @@ int main(int argc, char** argv) {
             uint16_t height = W;
             uint16_t x_coord = regs[Y] % DISPLAY_WIDTH;
             uint16_t y_coord = regs[Z] % DISPLAY_HEIGHT;
-            uint16_t sprite_addr = index_register;
 
             bool flipped_off = false;
 
-            for (uint16_t h = 0;h < height && y_coord + h < DISPLAY_HEIGHT; ++h) {
+            for (uint16_t h = 0; h < height && y_coord + h < DISPLAY_HEIGHT; ++h) {
                 unsigned char sprite_byte = mem[index_register + h];
                 unsigned int left_index = ((y_coord + h) * DISPLAY_WIDTH + x_coord) / 8;
-                
+
                 char old_left_byte = display[left_index];
                 char old_right_byte = display[left_index];
 
                 if (x_coord / 8 < DISPLAY_WIDTH / 8) {
-                    char left_byte_xor = display[left_index] ^ (sprite_byte >> (x_coord % 8));                
-                    display[left_index] = left_byte_xor;       
+                    char left_byte_xor = display[left_index] ^ (sprite_byte >> (x_coord % 8));
+                    display[left_index] = left_byte_xor;
                     flipped_off |= (old_left_byte & (old_left_byte ^ left_byte_xor)) != 0x0;
-                }   
-
-                if (x_coord / 8 + 1 < DISPLAY_WIDTH / 8) {
-                    char right_byte_xor = display[left_index + 1] ^ (sprite_byte << (8 - (x_coord % 8)));
-                    display[left_index + 1] = right_byte_xor;    
-                    flipped_off |= (old_right_byte & (old_right_byte ^ right_byte_xor)) != 0x0;
                 }
 
-            } 
+                if (x_coord / 8 + 1 < DISPLAY_WIDTH / 8) {
+                    char right_byte_xor =
+                        display[left_index + 1] ^ (sprite_byte << (8 - (x_coord % 8)));
+                    display[left_index + 1] = right_byte_xor;
+                    flipped_off |= (old_right_byte & (old_right_byte ^ right_byte_xor)) != 0x0;
+                }
+            }
 
             regs[0xF] = flipped_off;
         } else if (X == 0xF && Z == 0x5 && W == 0x5) {
 
             // FX55 -> Store registers from 0 to X to memory starting at I
-            for (unsigned int i = 0;i <= Y; ++i) {
+            for (unsigned int i = 0; i <= Y; ++i) {
                 mem[index_register + i] = regs[i];
             }
-                
+
         } else if (X == 0xF && Z == 0x6 && W == 0x5) {
 
             // FN65 -> Load registers from 0 to N from memory at I
-            for (unsigned int i = 0;i <= Y; ++i) {
+            for (unsigned int i = 0; i <= Y; ++i) {
                 regs[i] = mem[index_register + i];
             }
-                
+
         } else if (X == 0xF && Z == 0x3 && W == 0x3) {
 
             // FN33 -> convert regs[N] to 3 decimal digits and store under mem[I+1/2]
-            
+
             uint8_t num = regs[Y];
             mem[index_register] = num / 100;
             mem[index_register + 1] = (num % 100) / 10;
@@ -383,11 +431,67 @@ int main(int argc, char** argv) {
             // FN1E -> Add regs[N] to index register
             index_register += regs[Y];
 
+        } else if (X == 0xF && Z == 0x0 && W == 0xA) {
+
+            // FN0A -> Wait for a key press then store it in regs[N]
+            std::lock_guard<std::mutex> lock(key_mutex);
+            if (last_key_pressed == 0xFF) {
+                waiting_for_key = true;
+                program_counter -= 2;
+            } else {
+                regs[Y] = last_key_pressed;
+                last_key_pressed = 0xFF;
+                waiting_for_key = false;
+            }
+
+        } else if (X == 0xF && Z == 0x0 && W == 0x7) {
+
+            // FN07 -> set regs[N] to value of deley timer
+            regs[Y] = delay_timer;
+
+        } else if (X == 0xF && Z == 0x1 && W == 0x5) {
+
+            // FN15 -> set deley timer to regs[N]
+            delay_timer = regs[Y];
+
+        } else if (X == 0xF && Z == 0x1 && W == 0x5) {
+
+            // FN18 -> set the sound timer to regs[Y]
+            sound_timer = regs[Y];
+
+        } else if (X == 0x2) {
+
+            // 2NNN -> exec subroutine under NNN
+            stack.push(program_counter);
+            program_counter = (instruction & 0x0FFF);
+
+        } else if (instruction == 0x00EE) {
+
+            // 00EE -> return from subroutine
+            program_counter = stack.top();
+            stack.pop();
+
+        } else if (X == 0xE && Z == 0x9 && W == 0xE) {
+
+            // EN9E -> if key under regs[N] is pressed skip next instruction
+            std::lock_guard<std::mutex> lock(key_mutex);
+            if (keys_pressed[regs[Y]])
+                program_counter += 2;
+
+        } else if (X == 0xE && Z == 0xA && W == 0x1) {
+
+            // ENA1 -> if key under regs[N] is not pressed skip next instruction
+            std::lock_guard<std::mutex> lock(key_mutex);
+            if (!keys_pressed[regs[Y]])
+                program_counter += 2;
+
         } else {
-            std::cout << "Unrecognised instruction: " << std::hex << instruction << std::dec << std::endl;
+            std::cout << "Unrecognised instruction: " << std::hex << instruction << std::dec
+                      << std::endl;
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds((unsigned int)((1.0f / CLOCK_FPS) * 1000.0f)));
+        std::this_thread::sleep_for(
+            std::chrono::milliseconds((unsigned int)((1.0f / CLOCK_FPS) * 1000.0f)));
     }
 
     sdl_thread.join();
